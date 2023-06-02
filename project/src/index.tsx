7d04944ad256cc0977c06ce09da8530586811961
Viktoriia Sevostianova <vikapasha14@gmail.com>
2023-06-02 19:24:09 +0700

    add scrollToTop component

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,5 +1,5 @@
 import { HelmetProvider } from 'react-helmet-async';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { MainPage } from './pages/MainPage';
 import { NotFoundPage } from './pages/NotFoundPage';
@@ -8,40 +8,53 @@ import { AddReviewPage } from './pages/AddReviewPage';
 import { Player } from './components/Player';
 import { Login } from './pages/Login';
 import { PrivateRouter } from './components/PrivateRoute';
+import ScrollToTop from './components/ScrollToTop';
 import { films } from './mocks/films';
 import { APP_ROUTE, AUTHORIZATION_STATUS } from './constants';
 
+const ScrollToTopLayout = () => (
+  <>
+    <ScrollToTop />
+    <Outlet />
+  </>
+);
+
 const router = createBrowserRouter([
   {
-    path: APP_ROUTE.NotFound,
-    element: <NotFoundPage />,
-  },
-  {
-    path: APP_ROUTE.Root,
-    element: <MainPage films={films} />,
-  },
-  {
-    path: APP_ROUTE.Login,
-    element: <Login />,
-  },
-  {
-    path: APP_ROUTE.Film,
-    element:
-    <PrivateRouter authorizationStatus= {AUTHORIZATION_STATUS.Auth}>
-      <FilmDetailsPage />
-    </PrivateRouter>,
-  },
-  {
-    path: APP_ROUTE.Review,
-    element: <AddReviewPage />,
-  },
-  {
-    path: APP_ROUTE.Player,
-    element:
-    <PrivateRouter authorizationStatus= {AUTHORIZATION_STATUS.NoAuth}>
-      <Player/>
-    </PrivateRouter>,
-  },
+    element: <ScrollToTopLayout />,
+    children: [
+      {
+        path: APP_ROUTE.NotFound,
+        element: <NotFoundPage />,
+      },
+      {
+        path: APP_ROUTE.Root,
+        element: <MainPage films={films} />,
+      },
+      {
+        path: APP_ROUTE.Login,
+        element: <Login />,
+      },
+      {
+        path: APP_ROUTE.Film,
+        element:
+        <PrivateRouter authorizationStatus= {AUTHORIZATION_STATUS.Auth}>
+          <FilmDetailsPage />
+        </PrivateRouter>,
+      },
+      {
+        path: APP_ROUTE.Review,
+        element: <AddReviewPage />,
+      },
+      {
+        path: APP_ROUTE.Player,
+        element:
+        <PrivateRouter authorizationStatus= {AUTHORIZATION_STATUS.NoAuth}>
+          <Player/>
+        </PrivateRouter>,
+      },
+    ]
+  }
 ]);
 
 const root = document.getElementById('root');
